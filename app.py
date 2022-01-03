5f1c76364d12c634eb44950caee47fdc70720b4e
Mael <maelvittorio@gmail.com>
2022-01-03 17:32:50 +0100

    Comments added

--- app.py
+++ app.py
@@ -10,27 +10,32 @@ app = Flask(__name__)
 
 array1 = []
 
+#Default route
 @app.route("/",methods=["GET"])
 def func():
     return "ciao"
 
+#Route Predict 
 @app.route("/predict", methods=["POST"])
 def predict():
-    #get audio file and save it
+    #access to data in the post request
     data1 = request.json
-    
+    #Transform data in json format
     row = json.loads(data1)["data"]
+    #Extract the 7 parameters
     arr = row.split("@")
-    
+    #Add parameters to the global array
     array1.append(arr)
-
+    #Write the global array in local ssd
     np.savetxt("provacsv.csv",
            array1,
            delimiter =", ",
            fmt ='% s')
-    
-    return "ciao"
+    #Return a confirmation status
+    return "Tutto ok"
 
+#Flask default configuration
 if __name__ == "__main__" :
+    #0.0.0.0 is the default location (if the client is not che same machine see the ip of the server)
     app.run(debug=False, host= '0.0.0.0')
     #app.run(port=5000, debug=False)
--- client.py
+++ client.py
@@ -1,18 +1,14 @@
 import requests
 import json
 
+#Define URL to send data and door
 URL = "http://127.0.0.1:5000/predict"
-#URL = "https://score-rest.herokuapp.com/predict"
-#URL = "http://167.99.214.132/predict"
-
-#TEST_AUDIO_FILE = "AudioFIle/voce.wav"
 
 if __name__ == "__main__":
-    #files = {'file': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
+    #Test data
     data=json.dumps({"data" : "0@1@2@3@4@5@6"})
+    #Send request
     response = requests.post(URL, json=data )
-    
 
-    #data = response.json()
-
+    #Print response
     print(str(response))
