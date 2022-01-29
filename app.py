76a859525d75f082b750a8d8128f691f8c3f474d
Mael <maelvittorio@gmail.com>
2022-01-29 18:51:13 +0100

    Gitignore added

--- app.py
+++ app.py
@@ -1,19 +1,63 @@
-import random
-import os
 from flask import Flask, request, jsonify
 import json
 import numpy as np
+import datetime
+import socket
+import threading
+ 
 
+UDP_IP = "0.0.0.0"
+UDP_PORT = 65013
+BUFFER_SIZE = 128
 
 
 app = Flask(__name__)
 
 array1 = []
+array2 = []
+
+def udpFunc():
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+    conta = 0
+    while True:
+        data = sock.recvfrom(BUFFER_SIZE)
+        if data:  
+            #print(data, conta)         
+            row = data[0]
+            encoding = 'utf-8'
+            stringa = str(row, encoding=encoding)
+
+            arr = stringa.split("@")
+            
+            array1.append(arr)       
+            
+            if conta == 49:
+                np.savetxt("provacsv.csv",
+                    array1,
+                    delimiter =", ",
+                    fmt ='% s')
+                conta = 0
+            conta += 1
+
+    sock.close()
+
 
 #Default route
-@app.route("/",methods=["GET"])
+@app.route("/timestamp",methods=["GET"])
 def func():
-    return "ciao"
+    #take timestamp in seconds
+    ts = str(datetime.datetime.now().timestamp())
+    #take microseconds of timestamp in order to have time in format secondsFrom1970.microseconds
+    micros = str(datetime.datetime.now().microsecond)
+    return (ts, micros)
+
+@app.route("/streaming",methods=["GET"])
+def func2():
+    thread = threading.Thread(target=udpFunc)
+    thread.start()
+    return "Streaming partito"
 
 #Route Predict 
 @app.route("/predict", methods=["POST"])
@@ -21,16 +65,30 @@ def predict():
     #access to data in the post request
     data1 = request.json
     #Transform data in json format
+    data1 = json.dumps(data1 )
+
+
     row = json.loads(data1)["data"]
-    #Extract the 7 parameters
-    arr = row.split("@")
+    #Extract the 50 rows
+    arr = row.split("/")
+    #Extract the 7 parameters and add it to the matrix
+    matr = []
     #Add parameters to the global array
-    array1.append(arr)
-    #Write the global array in local ssd
+    for i in arr:
+        matr.append(i.split("@"))
+    
+    array1.append(matr)     
+
+    for i in array1:
+        for q in i :
+            array2.append(q)    
+    
     np.savetxt("provacsv.csv",
-           array1,
+           array2,
            delimiter =", ",
            fmt ='% s')
+
+    array2.clear()
     #Return a confirmation status
     return "Tutto ok"
 
